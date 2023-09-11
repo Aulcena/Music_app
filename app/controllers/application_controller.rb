fea315f71d07caca5de428b6c78848119dcf8cb9
AJU <aulcena@gmail.com>
2023-09-11 16:44:41 -0400

    added login functionality for user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+    
+    private
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_user_session_token!
+    end
+
+    def logout!
+        current_user.try(:reset_user_session_token!)
+        session[:session_token] = nil
+    end
 end
